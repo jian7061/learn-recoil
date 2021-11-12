20ec4641f7c54525a881a446d6414c2ff8378a5a
Jian Kim <jian7061@gmail.com>
2021-11-12 16:55:49 +0900

    practice recoil by creating an animal filter

--- recoil_todolist/src/App.tsx
+++ recoil_todolist/src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  atom,
+  selector,
+  RecoilRoot,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
 
-function App() {
+const ALL = "all";
+const ANIMALS = {
+  DOG: "dog",
+  CAT: "cat",
+};
+
+const animalsState = atom({
+  key: "animalsState",
+  default: [
+    {
+      id: 1,
+      name: "Rexy",
+      type: ANIMALS.DOG,
+    },
+    {
+      id: 2,
+      name: "Oscar",
+      type: ANIMALS.CAT,
+    },
+    {
+      id: 3,
+      name: "Tom",
+      type: ANIMALS.CAT,
+    },
+  ],
+});
+
+const animalFilterState = atom({
+  key: "animalFilterState",
+  default: ALL,
+});
+
+const filteredAnimalState = selector({
+  key: "animalListState",
+  get: ({ get }) => {
+    const filter = get(animalFilterState);
+    const animals = get(animalsState);
+    if (filter === ALL) return animals;
+    return animals.filter((animal) => animal.type === filter);
+  },
+});
+
+const Animals = () => {
+  const animals = useRecoilValue(filteredAnimalState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {animals.map((animal) => (
+        <div>
+          {animal.name} {animal.type}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default App;
+const PickAnimal = () => {
+  const setAnimalFilter = useSetRecoilState(animalFilterState);
+  return (
+    <div>
+      <button onClick={() => setAnimalFilter(ALL)}>All</button>
+      <button onClick={() => setAnimalFilter(ANIMALS.DOG)}>Dogs</button>
+      <button onClick={() => setAnimalFilter(ANIMALS.CAT)}>Cats</button>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <RecoilRoot>
+      <div>
+        <PickAnimal />
+        <Animals />
+      </div>
+    </RecoilRoot>
+  );
+}
