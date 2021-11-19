aa635f32150ed882e648fe5e4dbc0bb5cd15b90b
Jian Kim <jian7061@gmail.com>
2021-11-19 16:12:46 +0900

    add regular exp to validate email input data and presen error message to users

--- recoil_todolist/src/TodoList.tsx
+++ recoil_todolist/src/TodoList.tsx
@@ -34,23 +34,41 @@ export default function TodoList() {
         style={{ display: "flex", flexDirection: "column" }}
         onSubmit={handleSubmit(onValid)}
       >
-        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{formState.errors?.email?.message}</span>
         <input
           {...register("firstName", { required: true })}
           placeholder="First Name"
         />
+        <span>{formState.errors?.firstName?.message}</span>
+
         <input
           {...register("lastName", { required: true })}
           placeholder="Last Name"
         />
+        <span>{formState.errors?.lastName?.message}</span>
+
         <input
           {...register("username", { required: true, minLength: 10 })}
           placeholder="Username"
         />
+        <span>{formState.errors?.username?.message}</span>
+
         <input
           {...register("password", { required: true, minLength: 5 })}
           placeholder="Password"
         />
+        <span>{formState.errors?.password?.message}</span>
+
         <input
           {...register("password1", {
             required: "Password is required",
@@ -61,6 +79,8 @@ export default function TodoList() {
           })}
           placeholder="Password1"
         />
+        <span>{formState.errors?.password1?.message}</span>
+
         <button>Add</button>
       </form>
     </div>
