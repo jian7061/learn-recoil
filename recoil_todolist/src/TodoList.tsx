d6dc39eee402874f873015bd87a216823c093aa1
Jian Kim <jian7061@gmail.com>
2021-11-19 15:03:46 +0900

    how to deal with more than 2 inputs at the same time using React-Hook-Form : register, watch

--- recoil_todolist/src/TodoList.tsx
+++ recoil_todolist/src/TodoList.tsx
@@ -1,18 +1,37 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// export default function TodoList() {
+//   const [todo, setTodo] = useState("");
+//   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+//     setTodo(e.currentTarget.value);
+//   };
+//   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     console.log(todo);
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={todo} placeholder="write a to do" />
+//         <button>Add</button>
+//       </form>
+//     </div>
+//   );
+// }
 
 export default function TodoList() {
-  const [todo, setTodo] = useState("");
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setTodo(e.currentTarget.value);
-  };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(todo);
-  };
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} placeholder="write a to do" />
+      <form>
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("firstName")} placeholder="First Name" />
+        <input {...register("lastName")} placeholder="Last Name" />
+        <input {...register("userName")} placeholder="User Name" />
+        <input {...register("password")} placeholder="Password" />
+        <input {...register("password1")} placeholder="Password1" />
         <button>Add</button>
       </form>
     </div>
